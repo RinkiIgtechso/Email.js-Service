ec27b02553ff3e3b77af5a64d17a3ddceab584b7
RinkiIgtechso <kumaririnki@igtechso.com>
2023-12-04 17:37:47 +0530

    Sending email with email.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import "./App.css";
+
+const App = () => {
+ const form = useRef();
+ console.log(process.env.REACT_APP_EMAIL_JS_SERVICE_ID)
+
+ const sendEmail = (e) => {
+   e.preventDefault(); // prevents the page from reloading when you hit “Send”
+
+   emailjs.sendForm(`${process.env.REACT_APP_EMAIL_JS_SERVICE_ID}`, `${process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY}`)
+     .then((result) => {
+         // show the user a success message
+         console.log(result);
+     }, (error) => {
+         // show the user an error
+         console.log(error);
+     });
+ };
+
+ return (
+   <form ref={form} onSubmit={sendEmail} className='App'>
+    <div>
+      <label>Name</label> <br/>
+      <input type="text" name="user_name" />
+    </div>
+    <div>
+      <label>Email</label><br/>
+      <input type="email" name="user_email" />
+    </div>
+    <div>
+      <label>Message</label><br/>
+      <textarea name="message" />
+    </div>
+     <input type="submit" value="Send" />
+   </form>
+ );
+};
+
+export default App;
