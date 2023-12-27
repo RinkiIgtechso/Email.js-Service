37cde56278ae337cb8f656b15ac2841e411f18ad
RinkiIgtechso <kumaririnki@igtechso.com>
2023-12-27 11:42:42 +0530

    Added validation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import validator from 'validator';
 import "./App.css";
 
 const App = () => {
  const form = useRef();
- console.log(process.env.REACT_APP_EMAIL_JS_SERVICE_ID)
 
  const sendEmail = (e) => {
-   e.preventDefault(); // prevents the page from reloading when you hit “Send”
-
-   emailjs.sendForm(`${process.env.REACT_APP_EMAIL_JS_SERVICE_ID}`, `${process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY}`)
-     .then((result) => {
-         // show the user a success message
-         console.log(result);
-     }, (error) => {
-         // show the user an error
-         console.log(error);
-     });
+    e.preventDefault(); // prevents the page from reloading when you hit 
+    let validate = validator.isEmail(e.target[1].value)
+    if(validate){
+      emailjs.sendForm(`${process.env.REACT_APP_EMAIL_JS_SERVICE_ID}`, `${process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY}`)
+      .then((result) => {
+          // show the user a success message
+          console.log(result);
+      }, (error) => {
+          // show the user an error
+          console.log(error);
+      });
+    }
  };
 
  return (
    <form ref={form} onSubmit={sendEmail} className='App'>
     <div>
       <label>Name</label> <br/>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" required/>
     </div>
     <div>
       <label>Email</label><br/>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" required />
     </div>
     <div>
       <label>Message</label><br/>
-      <textarea name="message" />
+      <textarea name="message" required />
     </div>
      <input type="submit" value="Send" />
    </form>
